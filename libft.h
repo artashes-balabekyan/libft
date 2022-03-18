95a9cc2c5b1b87f6b86938d86cfdbbcba67c56b8
Artashes Balabekyan <balabekyanartashes@gmail.com>
2022-03-19 03:52:39 +0400

    feat: add functions ot libft.h

--- libft.h
+++ libft.h
@@ -27,5 +27,9 @@ void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void *ft_memmove(void *restrict dst, const void *restrict src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int ft_toupper(int c);
+int ft_tolower(int c);
+char *ft_strchr(const char *s, int c);
+char *ft_strrchr(const char *s, int c);
 
 #endif
