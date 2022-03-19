4b1ca9cbf596420d75cb3bbcad76ccf8eae34869
Artashes Balabekyan <balabekyanartashes@gmail.com>
2022-03-20 01:26:18 +0400

    fix: norm ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,15 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abalabek <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/20 01:23:39 by abalabek          #+#    #+#             */
+/*   Updated: 2022/03/20 01:23:41 by abalabek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./libft.h"
 #include <stdio.h>
 
-int ft_strncmp(const char *s1, const char *s2, t_size_t n)
+int	ft_strncmp(const char *s1, const char *s2, t_size_t n)
 {
-  t_size_t i = 0;
-  while (i < n)
-  {
-    if (s1[i] != s2[i]){
-      return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-    }
-    i++;
-  }
-  return (0);
+	t_size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
 }
