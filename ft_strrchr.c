72d53f9310167b5269ad808f8b47d732d15c3483
Artashes Balabekyan <balabekyanartashes@gmail.com>
2022-03-20 01:33:54 +0400

    fix: norm ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,16 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abalabek <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/20 01:26:30 by abalabek          #+#    #+#             */
+/*   Updated: 2022/03/20 01:26:31 by abalabek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./libft.h"
 #include <stdio.h>
 
-char *ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-  const char *found, *p;
-  if (c == '\0')
-    return ft_strchr (s, '\0');
-  found = NULL;
-  while ((p = ft_strchr (s, c)) != NULL)
-    {
-      found = p;
-      s = p + 1;
-    }
-  return (char *) found;
+	const char	*found;
+	const char	*p;
+
+	if (c == '\0')
+		return (ft_strchr (s, '\0'));
+	found = NULL;
+	p = ft_strchr (s, c);
+	while (p != NULL)
+	{
+		found = p;
+		s = p + 1;
+		p = ft_strchr (s, c);
+	}
+	return ((char *) found);
 }
