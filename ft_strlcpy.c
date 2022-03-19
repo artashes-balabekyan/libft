e0d4ced4b60b1487b9f61f85454432821c895c42
Artashes Balabekyan <balabekyanartashes@gmail.com>
2022-03-19 21:28:22 +0400

    feat: ft_strlcpy 0 dstsize case

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -4,11 +4,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	char *d = dst;
 	char *s = (char *)src;
+	size_t size = ft_strlen(s);
+	if (dstsize == 0)
+		return (size);
 	while(dstsize > 1 && *src)
 	{
 		*d++ = *src++;
 		dstsize--;
 	}
 	*d++ = 0;
-	return (ft_strlen(s));
+	return (size);
 }
