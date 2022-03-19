cc3efbd029ac28db96f41cbbf5be4c7e2a11d7cb
Artashes Balabekyan <balabekyanartashes@gmail.com>
2022-03-20 01:00:31 +0400

    fix: norm ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,13 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abalabek <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/20 00:54:15 by abalabek          #+#    #+#             */
+/*   Updated: 2022/03/20 00:54:17 by abalabek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "./libft.h"
 
-void *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-    char *destination = dst;
-    const char *source = src;
-    while (n--)
-    {
-        *destination++ = *source++;
-    }
-    return (dst);
+	char	*destination;
+	char	*source;
+
+	destination = dst;
+	source = (char *)src;
+	while (n--)
+	{
+		*destination++ = *source++;
+	}
+	return (dst);
 }
